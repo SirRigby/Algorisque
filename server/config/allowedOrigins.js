303b1a7a0b74e52c6131d33141cbd6e2aec01c3b
SirRigby <vanshkunwarji41@gmail.com>
2024-07-30 15:21:42 +0530

    done

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,12 @@
+const allowedOrigins = [
+    // 'https://www.yoursite.com',
+    // 'http://127.0.0.1:5500',
+    // 'http://localhost:3500',
+    'http://localhost:5173',
+    'http://codebuddyapp.netlify.app',
+    'https://codebuddyapp.netlify.app',
+    'http://codebuddyapp.tk/',
+    'https://codebuddyapp.tk/',
+];
+
+module.exports = allowedOrigins;
